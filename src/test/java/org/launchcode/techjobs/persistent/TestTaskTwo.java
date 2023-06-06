b677aed9eb8749684ec7e4c323c9d4e8a899efed
emjayholla <maggie.hollander@gmail.com>
2023-06-05 22:44:49 -0500

    Completed part 2 (models, data layer and controllers)

--- src/test/java/org/launchcode/techjobs/persistent/TestTaskTwo.java
+++ src/test/java/org/launchcode/techjobs/persistent/TestTaskTwo.java
@@ -16,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.launchcode.techjobs.persistent.controllers.SkillController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
