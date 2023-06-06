6c22f70e5e2820510327539ea01c17755adc433d
emjayholla <maggie.hollander@gmail.com>
2023-06-05 20:03:08 -0500

    Completed controllers #2. Working on #3.

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,6 +15,13 @@ import java.util.Optional;
 @RequestMapping("employers")
 public class EmployerController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    public String index(Model model) {
+        model.addAttribute("employers", employerRepository.findAll());
+        return "index";
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
